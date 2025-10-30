e5b531c3b5250d520097cb7593a4caf9b8e9bdb4
hansaka1996 <hansaka.18@itfac.mrt.ac.lk>
2025-10-30 23:44:39 +0530

    Add basename to BrowserRouter for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'; // Import this
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrap your App */}
+    <BrowserRouter basename="/qr-viewer">{/* Wrap your App */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
